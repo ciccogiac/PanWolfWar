5e6e23abbf33b91961a9d8535a1a3e09897bb823
cicciogiac <francy0898@gmail.com>
2024-05-27 15:20:35 +0200

    First Commit

---
+++ Source/PanWolfWar/PanWolfWarGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PanWolfWarGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APanWolfWarGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APanWolfWarGameMode();
+};
+
+
+
